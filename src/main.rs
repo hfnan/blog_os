bda35a7ae2c4be69a8352e21803e36867bdb1c01
hfnan <1278704684@qq.com>
2023-10-22 19:44:47 +0800

    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(blog_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer; 
-
+use blog_os::println;
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info);
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
